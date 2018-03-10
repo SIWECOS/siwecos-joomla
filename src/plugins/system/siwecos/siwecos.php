b31cfec23bc186beb495304b7e1a0ecff9056a35
Harald Leithner <joomla@itronic.at>
2018-03-10 17:36:27 +0100

    Initial commit.

---
+++ src/plugins/system/siwecos/siwecos.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package     SIWECOS.Plugin
+ *
+ * @copyright   Copyright (C) 2018 eco - Verband der Internetwirtschaft e.V., Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+/**
+ * SIWECOS System Plugin.
+ *
+ * @since  0.8
+ */
+class PlgSystemSiwecos extends JPlugin
+{
+	
+}
